ff572fa6b29dea63fe45e8ebfaccd62bf4ffb0b3
wyvie <irum@redhat.com>
2018-03-09 16:46:45 +0100

    fixed error messages for wiki handler

--- src/main/java/org/wyvie/chehov/bot/commands/helper/UrlHelper.java
+++ src/main/java/org/wyvie/chehov/bot/commands/helper/UrlHelper.java
@@ -1,11 +1,9 @@
 package org.wyvie.chehov.bot.commands.helper;
 
+import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -29,23 +27,6 @@ public class UrlHelper {
                         "AppleWebKit/537.11 (KHTML, like Gecko) " +
                         "Chrome/23.0.1271.95 Safari/537.11");
 
-        return toString(urlConnection.getInputStream());
-    }
-
-    private static String toString(InputStream inputStream) throws IOException
-    {
-        try (
-                BufferedReader bufferedReader =
-                        new BufferedReader(new InputStreamReader(inputStream, "UTF-8"))
-        ) {
-
-            String inputLine;
-            StringBuilder stringBuilder = new StringBuilder();
-            while ((inputLine = bufferedReader.readLine()) != null) {
-                stringBuilder.append(inputLine);
-            }
-
-            return stringBuilder.toString();
-        }
+        return IOUtils.toString(urlConnection.getInputStream());
     }
 }
--- src/main/java/org/wyvie/chehov/bot/commands/wiki/WikiCommand.java
+++ src/main/java/org/wyvie/chehov/bot/commands/wiki/WikiCommand.java
@@ -2,8 +2,11 @@ package org.wyvie.chehov.bot.commands.wiki;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
+import com.pengrad.telegrambot.model.User;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +14,7 @@ import org.springframework.util.StringUtils;
 import org.wyvie.chehov.bot.commands.CommandHandler;
 import org.wyvie.chehov.bot.commands.helper.UrlHelper;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -18,21 +22,26 @@ import java.util.regex.Pattern;
 @Service
 public class WikiCommand implements CommandHandler {
 
+    private final Logger logger = LoggerFactory.getLogger(WikiCommand.class);
+
     private static final String COMMAND = "wiki";
 
     private static final String URL_TEMPLATE="https://ru.wiktionary.org/w/index.php?printable=yes&" +
             "title=%TITLE%";
 
     private final TelegramBot telegramBot;
+    private final User botUser;
     private final UrlHelper urlHelper;
 
     private final Pattern pattern;
 
     @Autowired
     public WikiCommand(@Qualifier("telegramBot") TelegramBot telegramBot,
+                       @Qualifier("botUser") User botUser,
                        UrlHelper urlHelper) {
         this.telegramBot = telegramBot;
         this.urlHelper = urlHelper;
+        this.botUser = botUser;
 
         this.pattern = Pattern.compile("<span [^>]*id=\"Значение\">.*?(?=<ol>)" +
                 "<ol>(.*?(?=</ol>))</ol>");
@@ -47,8 +56,8 @@ public class WikiCommand implements CommandHandler {
     public void handle(Message message, String args) {
         if (StringUtils.isEmpty(args)) {
             sendMessage(message.chat().id(),
-                    "Пожалуйста, добавить предпет поиска.\n" +
-                            "Пример: /" + COMMAND + " собачка");
+                    "Пожалуйста, добавьте предмет поиска.\n" +
+                            "Пример: /" + COMMAND + "@" + botUser.username() + " собачка");
 
             return;
         }
@@ -58,13 +67,18 @@ public class WikiCommand implements CommandHandler {
         String source;
         try {
             source = urlHelper.getPageSource(url);
+        } catch (FileNotFoundException e) {
+            sendMessage(message.chat().id(), "Не нашёл. Отнюдь.");
+            return;
         } catch (IOException e) {
+            logger.error("io exception in wiki on page: " + url);
             sendMessage(message.chat().id(), "Извините, где-то я напутал и что-то пошло не так.");
             return;
         }
 
         Matcher matcher = pattern.matcher(source);
         if (!matcher.find()) {
+            logger.error("matcher error in wiki page: " + url);
             sendMessage(message.chat().id(), "Не нашёл. Отнюдь.");
             return;
         }
